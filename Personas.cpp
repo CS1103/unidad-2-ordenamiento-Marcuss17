d1bf1ababc4f09e8050c4ffb5baf1626ac380058
Marcuss17 <eduardo.medina@utec.edu.pe>
2019-09-30 14:45:40 -0500

    Manage to read file and create objects

---
+++ Personas.cpp
@@ -0,0 +1,5 @@
+//
+// Created by eduardo on 9/30/19.
+//
+
+#include "Personas.h"
---
+++ Personas.h
@@ -0,0 +1,14 @@
+//
+// Created by eduardo on 9/30/19.
+//
+
+#ifndef SORTMETHODS_PERSONAS_H
+#define SORTMETHODS_PERSONAS_H
+
+
+class Personas {
+
+};
+
+
+#endif //SORTMETHODS_PERSONAS_H
